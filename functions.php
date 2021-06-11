33e9d551c5094728294092afa5a61563a828bd26
Morgan Kay <morgan@wpalchemists.com>
2021-06-11 15:11:57 -0700

    version bump

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'FREEFROM_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'FREEFROM_VERSION', '0.6' );
+	define( 'FREEFROM_VERSION', '0.7' );
 }
 
 if ( ! function_exists( 'freefrom_setup' ) ) :
