0db7c051b704ec3496c08281ef30a49b526c98f7
Morgan Kay <morgan@wpalchemists.com>
2021-06-19 11:55:35 -0700

    UAT fixes

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'FREEFROM_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'FREEFROM_VERSION', '0.9' );
+	define( 'FREEFROM_VERSION', '0.9.1' );
 }
 
 if ( ! function_exists( 'freefrom_setup' ) ) :
